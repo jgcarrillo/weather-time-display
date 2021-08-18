f4f6accd15917ba8bd08c2e9be9a57e0e172bc7e
Jorge G <jorgegomezweb@gmail.com>
2021-08-18 16:19:10 +0200

    FIX: clock format when minutes are under 10

--- js/clock.js
+++ js/clock.js
@@ -12,6 +12,14 @@ const displayClock = () => {
 		month: date.getMonth(),
 	};
 
+	if (dateFormat.hours == 0) {
+		dateFormat.hours = 12;
+	}
+
+	dateFormat.hours = dateFormat.hours < 10 ? '0' + dateFormat.hours : dateFormat.hours;
+	dateFormat.minutes = dateFormat.minutes < 10 ? '0' + dateFormat.minutes : dateFormat.minutes;
+	dateFormat.seconds = dateFormat.seconds < 10 ? '0' + dateFormat.seconds : dateFormat.seconds;
+
 	contentHours.textContent = dateFormat.hours;
 	contentMinutes.textContent = dateFormat.minutes;
 	contentSeconds.textContent = dateFormat.seconds;
