fbb0dc5eae8038e76a0101bca840c77692459a30
Jorge G <jorgegomezweb@gmail.com>
2021-08-18 17:29:07 +0200

    FIX: correct day in weather.js

--- js/weather.js
+++ js/weather.js
@@ -4,6 +4,6 @@ const monthContent = document.querySelector('.month');
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 const month = months[date.getMonth()];
-const currentDay = date.getDay();
+const currentDay = date.getDate();
 
 monthContent.textContent = `${month} ${currentDay}`;
