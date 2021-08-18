284837a3f2aa4a56ab6ad251ca560b709074358e
Jorge G <jorgegomezweb@gmail.com>
2021-08-18 16:13:59 +0200

    FEAT: adding custom greetings based in actual time

--- js/config.js
+++ js/config.js
@@ -3,10 +3,10 @@ const CONFIG = {
 	name: 'Jorge',
 
 	// GREETINGS
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon!',
-	greetingEvening: 'Good evening!',
-	greetingNight: 'Good night!',
+	greetingMorning: 'Good morning',
+	greetingAfternoon: 'Good afternoon',
+	greetingEvening: 'Good evening',
+	greetingNight: 'Good night',
 
 	// WEATHER
 };
