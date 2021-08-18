e68406f97be3e883b7e6708c5bcffe4414d18c51
Jorge G <jorgegomezweb@gmail.com>
2021-08-18 17:24:42 +0200

    FIX: CSS for weather

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,9 @@
 const date = new Date();
+const monthContent = document.querySelector('.month');
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 const month = months[date.getMonth()];
-console.log(month);
+const currentDay = date.getDay();
+
+monthContent.textContent = `${month} ${currentDay}`;
