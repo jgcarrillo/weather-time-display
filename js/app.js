064a67eb4052379d7f7e3b547dcf3cd64abbab71
Jorge G <jorgegomezweb@gmail.com>
2021-08-18 15:54:46 +0200

    FEAT: basic clock display

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
-const date = new Date();
+const contentHours = document.querySelector('.clock__hours');
+const contentMinutes = document.querySelector('.clock__minutes');
+const contentSeconds = document.querySelector('.clock__seconds');
 
-const dateFormat = {
-	hours: date.getHours(),
-	minutes: date.getMinutes(),
-	month: date.getMonth(),
+const displayClock = () => {
+	const date = new Date();
+
+	const dateFormat = {
+		hours: date.getHours(),
+		minutes: date.getMinutes(),
+		seconds: date.getSeconds(),
+		month: date.getMonth(),
+	};
+
+	contentHours.textContent = dateFormat.hours;
+	contentMinutes.textContent = dateFormat.minutes;
+	contentSeconds.textContent = dateFormat.seconds;
+	setTimeout(displayClock, 1000);
 };
 
-console.log(dateFormat.month);
+displayClock();
