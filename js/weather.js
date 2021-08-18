0f69293a22e1738ec700eacdc29e4b4bee9e65c8
Jorge G <jorgegomezweb@gmail.com>
2021-08-18 16:24:24 +0200

    FEAT: Adding basic months array

---
+++ js/weather.js
@@ -0,0 +1,6 @@
+const date = new Date();
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const month = months[date.getMonth()];
+console.log(month);
