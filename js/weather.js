e88d3eb0e377a13fbf7b6e9a498571d0a4a16b1f
Jorge G <jorgegomezweb@gmail.com>
2021-08-19 10:43:27 +0200

    FEAT: adding icons

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,13 @@
 const date = new Date();
 const monthContent = document.querySelector('.month');
 const weatherContent = document.querySelector('.weather');
+const iconContent = document.querySelector('.icon');
 
 const key = `${API.weatherKey}`;
 const lat = `${API.weatherLatitude}`;
 const long = `${API.weatherLongitude}`;
 const tempUnit = `${API.weatherUnit}`;
+const icon = `${API.weatherIcon}`;
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -26,9 +28,16 @@ const getWeather = async () => {
 	const weather = {
 		state: data.weather[0].description,
 		temp: tempUnit == 'C' ? celsius + '°C' : (celsius * 9) / 5 + 32 + 'K',
+		icon: data.weather[0].icon,
 	};
 
+	const $img = document.createElement('img');
+	$img.setAttribute('src', `assets/icons/${icon}/${weather.icon}.png`);
+	$img.setAttribute('alt', 'weather icon');
+
+	iconContent.appendChild($img);
 	weatherContent.textContent = `${weather.state} - ${weather.temp}`;
+	console.log(data);
 };
 
 getWeather();
--- key.example.js
+++ key.example.js
@@ -4,4 +4,5 @@ const API = {
 	weatherUnit: 'C', // Between Celsius or Kelvin
 	weatherLatitude: '', // Paste here your latitude based on your location
 	weatherLongitude: '', // Paste here your longitude based on your location
+	weatherIcon: 'OneDark', // Between Nord, Dark, White
 };
