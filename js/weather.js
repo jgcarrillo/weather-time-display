ecc7e226ec3e3bcfe62ecda4f9c91e485be9efc0
Jorge G <jorgegomezweb@gmail.com>
2021-08-19 16:38:06 +0200

    FEAT: Adding try/catch in weather to display err when app doesn't have API key

--- js/weather.js
+++ js/weather.js
@@ -22,25 +22,31 @@ monthContent.textContent = `${month} ${currentDay}`;
 
 // FETCH DATA FUNCTION -- RECEIVING DATA FROM THE API
 const getWeather = async () => {
-	const res = await fetch(url);
-	const data = await res.json();
+	try {
+		const res = await fetch(url);
+		const data = await res.json();
 
-	// Conversion to grades
-	const KELVIN = 273.15;
-	const celsius = Math.floor(data.main.temp - KELVIN);
+		// Conversion to grades
+		const KELVIN = 273.15;
+		const celsius = Math.floor(data.main.temp - KELVIN);
 
-	const weather = {
-		state: data.weather[0].description,
-		temp: tempUnit == 'C' ? celsius + '°C' : (celsius * 9) / 5 + 32 + 'K',
-		icon: data.weather[0].icon,
-	};
+		const weather = {
+			state: data.weather[0].description,
+			temp: tempUnit == 'C' ? celsius + '°C' : (celsius * 9) / 5 + 32 + 'K',
+			icon: data.weather[0].icon,
+		};
 
-	const $img = document.createElement('img');
-	$img.setAttribute('src', `assets/icons/${icon}/${weather.icon}.png`);
-	$img.setAttribute('alt', 'weather icon');
+		const $img = document.createElement('img');
+		$img.setAttribute('src', `assets/icons/${icon}/${weather.icon}.png`);
+		$img.setAttribute('alt', 'weather icon');
 
-	iconContent.appendChild($img);
-	weatherContent.textContent = `${weather.state} - ${weather.temp}`;
+		iconContent.appendChild($img);
+		weatherContent.textContent = `${weather.state} - ${weather.temp}`;
+	} catch (err) {
+		console.log(
+			'You need to set your personal API key in file key.js. Then set your latitude and longitude -> https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}'
+		);
+	}
 };
 
 getWeather();
