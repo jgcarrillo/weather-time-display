63563914ef79613fd7baadf0b6a8e2e1a892ec8d
Jorge G <jorgegomezweb@gmail.com>
2021-08-19 10:17:06 +0200

    FEAT: API call function

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,11 @@
 const date = new Date();
 const monthContent = document.querySelector('.month');
+const weatherContent = document.querySelector('.weather');
+
+const key = `${API.weatherKey}`;
+const lat = `${API.weatherLatitude}`;
+const long = `${API.weatherLongitude}`;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
@@ -7,3 +13,20 @@ const month = months[date.getMonth()];
 const currentDay = date.getDate();
 
 monthContent.textContent = `${month} ${currentDay}`;
+
+const getWeather = async () => {
+	const res = await fetch(url);
+	const data = await res.json();
+
+	const KELVIN = 273.15;
+	const celsius = Math.floor(data.main.temp - KELVIN);
+
+	const weather = {
+		state: data.weather[0].description,
+		temp: celsius,
+	};
+
+	weatherContent.textContent = `${weather.state} - ${weather.temp}°C`;
+};
+
+getWeather();
